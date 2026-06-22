8961e6087a257c4af63595fd7de0a30b1357e161
Wikalot <275708108+Wikalot@users.noreply.github.com>
2026-06-22 16:44:04 +0300

    Make indentation and spacing consistent and remove comments

--- js.js
+++ js.js
@@ -1,8 +1,7 @@
-// Create function getComputerChoice 
 function getComputerChoice() {
-    // Get random variable between 1 (including) and 3 (including) using Math.random() and floor functions
+
     let random = Math.floor( Math.random() * 3 ) + 1;
-    // Create conditional statements that depending on the random variable return rock or paper or scissors
+
     if (random === 1) {
         return "rock";
     }
@@ -12,77 +11,77 @@ function getComputerChoice() {
     if (random === 3) {
         return "scissors";
     }
+
 }
 
-// Create function getHumanChoice 
 function getHumanChoice() {
-    // Prompt the user to get the users choice and store it in a variable called humanChoice
+
     let humanChoice = prompt("Choose between rock, paper, scissors:", "");
-    // Return the humans choice
     return humanChoice;
+
 }
 
-// Create function playGame
 function playGame() {
-    // Create humanScore and computerScore variables
+
     let humanScore = 0;
     let computerScore = 0;
 
-    // Create function playRound with parametrs humanChoice and computerChoice
     function playRound(humanChoice, computerChoice) {
-        // Convert humanChoice to lowercase
+
         humanChoice = humanChoice.toLowerCase();
-        // Make conditionals representing the ways the human can lose or win
+
         if (humanChoice === "rock" && computerChoice === "rock") {
             window.alert("Tie.");
-            return "Tie.";
+            return;
         }
         if (humanChoice === "rock" && computerChoice === "paper") {
             window.alert("You lose. Paper beats rock.");
             computerScore++;
-            return "You lose. Paper beats rock.";
+            return;
         }
         if (humanChoice === "rock" && computerChoice === "scissors") {
             window.alert("You win. Rock beats scissors.");
             humanScore++;
-            return "You win. Rock beats scissors.";
+            return;
         }
         if (humanChoice === "paper" && computerChoice === "rock") {
             window.alert("You win. Paper beats rock.");
             humanScore++;
-            return "You win. Paper beats rock.";
+            return;
         }
         if (humanChoice === "paper" && computerChoice === "paper") {
             window.alert("Tie");
-            return "Tie";
+            return;
         }
         if (humanChoice === "paper" && computerChoice === "scissors") {
             window.alert("You lose. Scissors beats rock.");
             computerScore++;
-            return "You lose. Scissors beats rock.";
+            return;
         }
         if (humanChoice === "scissors" && computerChoice === "rock") {
             window.alert("You lose. Rock beats scissors.");
             computerScore++;
-            return "You lose. Rock beats scissors.";
+            return;
         }
         if (humanChoice === "scissors" && computerChoice === "paper") {
             window.alert("You win. Scissors beats paper.");
             humanScore++;
-            return "You win. Scissors beats paper.";
+            return;
         }
         if (humanChoice === "scissors" && computerChoice === "scissors") {
             window.alert("Tie.");
-            return "Tie.";
+            return;
         }
+
     }
-    // Create loop that plays 5 rounds
+
     for(let i = 0; i < 5; i++){
-        // Create humanSelection and computerSelection variables
+   
         const humanSelection = getHumanChoice();
         const computerSelection = getComputerChoice();
 
         playRound(humanSelection, computerSelection);
+
     }
 
     if (humanScore > computerScore) {
@@ -95,6 +94,3 @@ function playGame() {
         window.alert(`Tie! Your score: ${humanScore}, computer score: ${computerScore}`);
     }
 }
-
-
-
